5d04cb0a7426994055c669e36d2fd7c51b7338b4
CJ Avilla <cjavilla@stripe.com>
2022-02-03 11:41:59 -0500

    update devise views to rails 7

--- db/migrate/20220203143833_add_stripe_id_to_payment_link.rb
+++
@@ -1,5 +0,0 @@
-class AddStripeIdToPaymentLink < ActiveRecord::Migration[7.1]
-  def change
-    add_column :payment_links, :stripe_id, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 2022_02_03_153259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "payment_links", force: :cascade do |t|
-    t.string "url"
-    t.string "price"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "stripe_id"
-    t.index ["user_id"], name: "index_payment_links_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -44,5 +34,4 @@ ActiveRecord::Schema.define(version: 2022_02_03_153259) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "payment_links", "users"
 end
